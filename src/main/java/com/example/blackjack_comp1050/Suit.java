788b4067c35354554eb50261ee7e8fdd8153f757
MCampbell720 <campbellm7@wit.edu>
2023-04-08 10:16:44 -0400

    Updated Suit Class

--- src/main/java/com/example/blackjack_comp1050/Suit.java
+++ src/main/java/com/example/blackjack_comp1050/Suit.java
@@ -1,4 +1,27 @@
 package com.example.blackjack_comp1050;
 
-public class Suit {
+public final class Suit {
+
+    //Variables
+    private String name;
+    private String symbol;
+
+    //Constructors
+    private Suit( String nameValue, String symbolValue ) {
+        name = nameValue;
+        symbol = symbolValue;
+    }
+    //Initialize the four suits
+    public final static Suit CLUB = new Suit("Clubs", "c");
+    public final static Suit SPADE = new Suit("Spades", "s");
+    public final static Suit HEART = new Suit("Hearts", "h");
+    public final static Suit DIAMONDS = new Suit("Diamonds", "d");
+
+    //Methods
+    public String getName() {
+        return name;
+    }
+    public String getSymbol() {
+        return symbol;
+    }
 }
