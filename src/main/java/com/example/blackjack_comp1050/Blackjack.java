cad0f687bcdb95f81db028ef2a10f70a96517433
MCampbell720 <campbellm7@wit.edu>
2023-04-09 00:02:26 -0400

    Blackjack class updates

--- src/main/java/com/example/blackjack_comp1050/Blackjack.java
+++ src/main/java/com/example/blackjack_comp1050/Blackjack.java
@@ -1,4 +1,9 @@
 package com.example.blackjack_comp1050;
 
 public class Blackjack {
+    //Main class that holds the gui and blackjack logic
+    private final Deck deck = new Deck(1);
+    private final Player player = new Player();
+    private final Player dealer = new Player();
+
 }
